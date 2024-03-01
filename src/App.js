afd31852c43706da11844017cfdde02cc38ff80a
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-03-01 14:21:41 +0530

    Done the Scrolling part...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+// import useRef hook
+import { useRef } from 'react';
+
 import './App.css';
 
 function App() {
+  // initialize useRef hook
+  const services = useRef(null);
+  const blog  = useRef(null);
+  const contact = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <div className="App">
       <div className='hero'>
         <ul>
-          <li className='link'>Services</li>
-          <li className='link'>Blog</li>
-          <li className='link'>Contact</li>
+          <li onClick={() => scrollToSection(services)} className='link'>Services</li>
+          <li onClick={() => scrollToSection(blog)} className='link'>Blog</li>
+          <li onClick={() => scrollToSection(contact)} className='link'>Contact</li>
         </ul>
       </div>
-      <div className='services'>Services</div>
-      <div className='blog'>Blog</div>
-      <div className='contact'>Contact</div>
+      <div ref={services} className='services'>Services</div>
+      <div ref={blog} className='blog'>Blog</div>
+      <div ref={contact} className='contact'>Contact</div>
     </div>
   );
 }
---
+++ src/Components/ScrollToTop.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+const ScrollToTop = () => {
+    return (
+        <div>ScrollToTop</div>
+    )
+}
+
+export default ScrollToTop
